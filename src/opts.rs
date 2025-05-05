eaf8f5a2a3056f8fbcb92d84b8b86a03007e102b
techyangj <1066900355@qq.com>
2025-05-05 21:48:51 +0800

    generate a random number

--- src/opts.rs
+++ src/opts.rs
@@ -15,6 +15,8 @@ pub struct Opts {
 pub enum SubCommand {
     #[command(name = "csv", about = "Convert CSV to other formats")]
     Csv(CsvOpts),
+    #[command(name = "genpass", about = "Generate a random password")]
+    GenPass(GenPassOpts),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -39,6 +41,20 @@ pub struct CsvOpts {
     pub header: bool,
 }
 
+#[derive(Debug, Parser)]
+pub struct GenPassOpts {
+    #[arg(short, long, default_value_t = 16)]
+    pub length: u8,
+    #[arg(long, default_value_t = true)]
+    pub uppercase: bool,
+    #[arg(long, default_value_t = true)]
+    pub lowercase: bool,
+    #[arg(long, default_value_t = true)]
+    pub number: bool,
+    #[arg(long, default_value_t = true)]
+    pub symbol: bool,
+}
+
 fn verify_input_file(file_name: &str) -> Result<String, &'static str> {
     if Path::new(file_name).exists() {
         Ok(file_name.into())
@@ -51,6 +67,7 @@ fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
     format.parse::<OutputFormat>()
 }
 
+// impl From 常用于类型之间的转换，实现了From<T> for U,自动会有Into<U> for T,可以使用into()
 impl From<OutputFormat> for &'static str {
     fn from(format: OutputFormat) -> Self {
         match format {
---
+++ src/process/gen_pass.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use rand::Rng;
+
+const UPPER: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWER: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+const NUMBER: &[u8] = b"0123456789";
+const SYMBOL: &[u8] = b"+-!@#$%^&*()_+<>/?_";
+
+pub fn process_genpass(
+    length: u8,
+    upper: bool,
+    lower: bool,
+    number: bool,
+    symbol: bool,
+) -> Result<()> {
+    let mut rng = rand::rng();
+    let mut password = String::new();
+    let mut chars = Vec::new();
+
+    if upper {
+        chars.extend_from_slice(UPPER);
+    }
+    if lower {
+        chars.extend_from_slice(LOWER);
+    }
+    if number {
+        chars.extend_from_slice(NUMBER);
+    }
+    if symbol {
+        chars.extend_from_slice(SYMBOL);
+    }
+
+    for _ in 0..length {
+        let idx = rng.random_range(0..chars.len());
+        password.push(chars[idx] as char);
+    }
+
+    println!("{}", password);
+    Ok(())
+}
